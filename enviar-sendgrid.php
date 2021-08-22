4a7aa8cd6569452740bd8794c1021e6339f30a7a
Henrique Black <67487181+hnrqblck@users.noreply.github.com>
2021-08-22 11:14:47 -0300

    PHP + SEO

---
+++ enviar-sendgrid.php
@@ -0,0 +1,36 @@
+<?php
+
+require("./sendgrid-php/sendgrid-php.php");
+
+$email_site = "contato@bikcraft.com";
+$nome_site = "Bikcraft";
+
+$email_user = $_POST["email"];
+$nome_user = $_POST["nome"];
+
+$body_content = "";
+foreach( $_POST as $field => $value) {
+  if( $field !== "leaveblank" && $field !== "dontchange" && $field !== "enviar") {
+    $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+    $body_content .= "$field: $value \n";
+  }
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject("Formulário Bikcraft");
+$email->addContent("text/plain", $body_content);
+
+$sendgrid = new \SendGrid("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");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo "Caught exception: ". $e->getMessage() ."\n";
+}
